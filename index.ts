837bd9437ab4710b80b2f94776529b473df56e1f
Ioannis Brant-Ioannidis <giannisbrant@gmail.com>
2023-03-31 23:17:22 +0400

    Removing simulations and adding default route

--- index.ts
+++ index.ts
@@ -3,18 +3,18 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import outagesRoutes from './routes/outages'
+import outagesRoutes from './routes/outagesRoutes'
 
 const app = express();
-const HOST = '127.0.0.1';
-const PORT = 3001;
+const PORT = 3000;
 app.use(express.urlencoded({extended: false})); 
 app.use(express.json());
 app.use(bodyParser.json());
 
 /* Route Handlers */
+app.use('/', () => "Black Out API");
 app.use('/outages',outagesRoutes);
-app.listen(PORT, HOST, () => console.log("Started listening"));
+app.listen(PORT, () => console.log("Started listening"));
 
 module.exports = app;
 export default app;
